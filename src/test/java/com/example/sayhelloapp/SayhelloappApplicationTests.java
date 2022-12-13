8b8875d691d7f856317db5c851e666f9be64e974
Anupkumr Seth <anupseth11@yahoo.com>
2022-12-13 09:51:54 +0100

    FirstCommit

---
+++ src/test/java/com/example/sayhelloapp/SayhelloappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sayhelloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayhelloappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
